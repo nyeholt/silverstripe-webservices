26e32b26cbc6bdbc0b001d7d02c166de56a851e3
Marcus Nyeholt <marcus@silverstripe.com.au>
2012-02-22 22:47:10 +1100

    Shortcuts for calling a webservice

---
+++ javascript/webservices.js
@@ -0,0 +1,21 @@
+
+;(function ($) {
+	window.SSWebServices = (function () {
+		var securityId = $('#SecurityID').val();
+		
+		var getService = function (name, method, params, cb) {
+			params['SecurityID'] = securityId;
+			$.get('jsonservice/' + name + '/' + method, params, cb);
+		}
+		
+		var postService = function (name, method, params, cb) {
+			params['SecurityID'] = securityId;
+			$.post('jsonservice/' + name + '/' + method, params, cb);
+		}
+		
+		return {
+			get: getService,
+			post: postService
+		};
+	})();
+})(jQuery);
