b9f71d56f9a10034aad2e244710b3ba30813c849
Marcus Nyeholt <nyeholt@gmail.com>
2013-09-25 16:27:28 +1000

    API Reworked token management
    Generated API token now only exists in the CMS view for the life of the
    session, and only the hash of the token is stored in the CMS. A regenerate
    checkbox is included that once triggered will continue to display the token,
    but only until the user's session is cleared.

--- code/extensions/TokenAccessible.php
+++ code/extensions/TokenAccessible.php
@@ -8,18 +8,69 @@
  */
 class TokenAccessible extends DataExtension {
 	private static $db = array(
-		'Token'				=> 'Varchar(32)',
 		'Active'			=> 'Boolean',
-		'AuthPrivateKey'	=> 'Varchar(64)',
+		'Token'				=> 'Varchar(128)',
+		'AuthPrivateKey'	=> 'Varchar(128)',
+		'RegenerateTokens'	=> 'Boolean',
 	);
 	
 	public function onBeforeWrite() {
+		
 		if (!$this->owner->Token) {
-			$this->owner->Token = md5(md5(uniqid() . mt_rand(0, 9000000)) . md5(microtime(true) . uniqid() . mt_rand(0, 9000000)));
+			$this->owner->RegenerateTokens = true;
 		}
 
 		if (!$this->owner->AuthPrivateKey) {
-			$this->owner->AuthPrivateKey = md5(microtime(true) . uniqid() . mt_rand(0, 9100000)) . md5(uniqid() . mt_rand(0, 900000));
+			$this->owner->RegenerateTokens = true;
 		}
+	}
+
+	public function updateCMSFields(\FieldList $fields) {
+		parent::updateCMSFields($fields);
+		
+		$token = $this->userToken();
+		
+		if (!$token) {
+			$token = "This user token can no longer be displayed - if you do not know this value, regenerate tokens by selecting Regenerate below";
+		} else {
+			$token = $this->owner->ID . ':' . $token;
+		}
+
+		$readOnly = ReadonlyField::create('DisplayToken', 'Token', $token);
+		$fields->addFieldToTab('Root.Main', $readOnly, 'AuthPrivateKey');
+		
+		$field = $fields->dataFieldByName('AuthPrivateKey');
+		$fields->replaceField('AuthPrivateKey', $field->performReadonlyTransformation());
+		
+		$fields->removeByName('Token');
+	}
+	
+	public function onAfterWrite() {
+		if ($this->owner->RegenerateTokens) {
+			$this->owner->RegenerateTokens = false;
+			$this->generateTokens();
+			$this->owner->write();
+		}
+	}
+	
+	/**
+	 * Generate and store the authentication tokens required
+	 * 
+	 * @TODO Rework this, it's not really any better than storing text passwords
+	 */
+	public function generateTokens() {
+		$generator = new RandomGenerator();
+		$token = $generator->randomToken('sha1');
+		$this->owner->Token = $this->owner->encryptWithUserSettings($token);
+		
+		// store the new token so it can be displayed later
+		Session::set('member_auth_token_' . $this->owner->ID, $token);
+
+		$authToken = $generator->randomToken('whirlpool');
+		$this->owner->AuthPrivateKey = $authToken;
+	}
+	
+	public function userToken() {
+		return Session::get('member_auth_token_' . $this->owner->ID);
 	}
 }
